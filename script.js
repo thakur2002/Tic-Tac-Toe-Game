311b70298534bb2cee45b328e4639e0882c04857
Thakurgupta <guptathakur87@gmail.com>
2024-08-04 12:39:19 +0530

    First commit  of tic tac toe

---
+++ script.js
@@ -0,0 +1,89 @@
+const statusDisplay = document.getElementById('status');
+const cells = document.querySelectorAll('.cell');
+const board = document.getElementById('board');
+const resetButton = document.getElementById('resetButton');
+
+let currentPlayer = 'X';
+let gameActive = true;
+let gameState = ['', '', '', '', '', '', '', '', ''];
+
+const xsound=new Audio("./sounds/blue.mp3");
+const osound=new Audio("./sounds/green.mp3");
+const wrongsound=new Audio("./sounds/wrong.mp3");
+const resetsound=new Audio("./sounds/yellow.mp3");
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
+
+const handlePlayerChange = () => {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    statusDisplay.innerText = `Player ${currentPlayer}'s turn`;
+};
+
+const handleResultValidation = () => {
+    let roundWon = false;
+    for (let i = 0; i < winningConditions.length; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerText = `Player ${currentPlayer} wins!`;
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes('');
+    if (roundDraw) {
+        statusDisplay.innerText = 'Game is a draw!';
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+};
+
+const handleCellClick = (event) => {
+    const clickedCell = event.target;
+    const clickedCellIndex = Array.from(cells).indexOf(clickedCell);
+
+    if (gameState[clickedCellIndex] !== '' || !gameActive) {
+        wrongsound.play();
+        return;
+    }
+    currentPlayer === 'X' ? xsound.play() : osound.play();
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerText = currentPlayer;
+    handleResultValidation();
+};
+
+const handleRestartGame = () => {
+    resetsound.play();
+    gameActive = true;
+    currentPlayer = 'X';
+    gameState = ['', '', '', '', '', '', '', '', ''];
+    statusDisplay.innerText = `Player ${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.innerText = '');
+};
+
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+resetButton.addEventListener('click', handleRestartGame);
